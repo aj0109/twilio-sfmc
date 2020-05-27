9a361a3346f591260c815c99f0e78b2635abc670
aj0109 <ashrayvjoshi@gmail.com>
2020-05-27 20:50:18 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -86,7 +86,7 @@ exports.execute = function (req, res) {
           .create({
          from: 'whatsapp:+14155238886',
          body: `Hello, ${to}`,
-         to: 'whatsapp:+917259862907'
+         to: 'whatsapp:+919000730637'
        })
           .then(message => console.log(message.sid)) 
           .done();
